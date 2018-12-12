16e60f1aadb4a480188be9a7d57934b337011e54
japan popotam <popotamjapan@gmail.com>
2018-12-13 07:24:59 +0900

    コメント追加など。

--- sudoku.py
+++ sudoku.py
@@ -15,21 +15,26 @@ def solve(cells):
   )
   model = cp_model.CpModel()
 
+  # 9x9=81このセルを変数で表現する。
+  # すでに値がセットされている値はその値のみ、そうでない場合は１から9までのいずれかの値をとる。
   variables = [ [ model.NewIntVar(1, 9, f'cell{x}{y}') if array[y][x] == 0 else model.NewIntVar(array[y][x], array[y][x], f'cell{x}{y}') for x in range(9)] for y in range(9)]
 
-  for y in range(0, 9):
+  # 制約を設定する。
+  # 横１行で数値の重複ができないことの制約を追加
+  for y in range(9):
     model.AddAllDifferent(variables[y])
-  for x in range(0, 9):
-    model.AddAllDifferent([ variables[y][x] for y in range(0, 9)])
+  # 縦１列で数値の重複ができないことの制約を追加
+  for x in range(9):
+    model.AddAllDifferent([ variables[y][x] for y in range(9) ])
+  # sub_block_mask で同じ値のセル同士は数値の重複が出来ないことの制約を追加
+  for s in range(9):
+    model.AddAllDifferent([ variables[y][x] for x in range(9) for y in range(9) if sub_block_mask[y][x] == s ])
 
-  for s in range(0, 9):
-    model.AddAllDifferent([ variables[y][x] for x in range(0, 9) for y in range(0, 9) if sub_block_mask[y][x] == s])
-
-
+  # ソルバーを生成して、上記の制約を満たす解を求める。
   solver = cp_model.CpSolver()
   status = solver.Solve(model)
   if status == cp_model.FEASIBLE:
-    return [ [ solver.Value(variables[y][x]) for x in range(0, 9) ] for y in range(0, 9) ]
+    return [ [ solver.Value(variables[y][x]) for x in range(9) ] for y in range(9) ]
   else:
     return []
 
@@ -50,7 +55,5 @@ if __name__ == '__main__':
 
   ans = solve(array)
 
-  for y in range(0, 9):
-    for x in range(0, 9):
-      print(f'{ans[y][x]}', end="")
-    print()
+  for rows in ans:
+    print("".join(map(str, rows)))
